e040336d75de3acf190f3be258093c24f4b9b246
Nathanael-Tekalign <nathanaeltekalign89@gmail.com>
2024-01-20 20:07:56 -0600

    week2 -- testerFile.js
    Using for loop and console.log.

---
+++ testerFile.js
@@ -0,0 +1,15 @@
+/*
+Author: Nathanael Tekalign
+Date: Janurary 14th, 2024
+*/
+var myVariableInteger_0 = 57;
+var myVariableInteger_0 = myVariableInteger_0 + 1;
+let myVariableString_0 = myVariableInteger_0.toString();
+let favoriteThing = prompt("What is your favorite thing?");
+console.log('myVariableString_0:', myVariableString_0);
+console.log('favoriteThing:', favoriteThing);
+for (let i = 0; i < 5; i++) {
+  if (i % 2 !== 0) {
+  console.log(i);
+  }
+}
