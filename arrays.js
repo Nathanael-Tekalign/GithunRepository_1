08fde5e7286a4677d50edcb34b09a505176e9afc
Nathanael-Tekalign <nathanaeltekalign89@gmail.com>
2024-01-27 21:54:51 -0600

    Add to Arrays

--- arrays.js
+++ arrays.js
@@ -1,18 +1,23 @@
 const Array1 = [0,1,2,3];
-const addOne = function(x) {
-  return x + 1;
+
+// Step 4:
+function addOne(x) {
+    console.log(++x);
 }
-var addOne_String = addOne.toString();
-console.log('addOne_String', addOne_String);
-function myFunction(func,addOne) {
-  if(addOne % 2 === 0) {
-    func === addOne;
-  }
-  else {
+// var addOne_String = addOne().toString();
+// console.log('addOne_String', addOne_String);
+
+// Step 5:
+function myFunction(theOtherFunction,x) {
+    if(x % 2 === 0) {
+        theOtherFunction(x);
+        return;
+    }
     console.log("this number is odd")
-  }
 }
-let txt = "";
-for(let x in Array1){
-  txt += Array1[x];
+
+// Step 6:
+for(let i in Array1){
+  myFunction(addOne,i)
 }
+
---
+++ test.py
@@ -0,0 +1,8 @@
+def newFunction(thisParameter, nati):
+    print(nati)
+    return thisParameter
+
+
+newString = newFunction("hello", "second parameter")
+print(newString)
+
