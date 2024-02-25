f59b8fc901878a6d8d86a3a3d04a5fdd38e7ee94
Nathanael-Tekalign <nathanaeltekalign89@gmail.com>
2024-02-24 18:10:19 -0600

    yes

---
+++ Unit2/lib_rary.js
@@ -0,0 +1,58 @@
+
+// Define a new class in lib_rary.js
+class Library_1{
+    // Define global "properties" of class Library_1:
+    lib_name;
+    //books = [];
+
+    // Constructor method that takes a `name` parameter
+    constructor(lib_name) {
+      // Set the name property of new instances to the name given
+      this.lib_name = lib_name;
+    }
+  
+    // Method that logs the value of the name property to the console
+    logName() {
+      console.log(this.lib_name);
+    }
+  }
+
+
+
+/*
+* a. the class should contain a constructor method that takes a name parameter.
+* b. The constructor should set the name property of a new instance to the name given through the paramenters.
+* c. The class should have a method that logs the value of the name property of the object to console.
+*/
+
+
+
+// class Book {
+//     title;
+//     author;
+//     genre;
+// }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Create a new instance of the class with the name "Alice"
+  //let alice = new Library_1("Alice");
+  
+  // Call the logName method on the instance
+  //alice.logName(); 
+  // Prints "Alice" to the console
---
+++ Unit2/objects.js
@@ -0,0 +1,31 @@
+// Import the library module
+//const library = require('./lib_rary.js');
+
+var jellyfish = new Library_1("Downtown Library");
+var starfish = new Library_1("Mall Library");
+
+
+jellyfish.logName();
+starfish.logName();
+
+
+
+
+// Create two objects of the class Book
+// let book1 = new lib_rary.Book('The Catcher in the Rye', 'J.D. Salinger', 'Fiction');
+// let book2 = new lib_rary.Book('The Hitchhiker\'s Guide to the Galaxy', 'Douglas Adams', 'Science Fiction');
+// let book3 = new lib_rary.Book()
+
+// Print the names of the books
+// console.log(book1.name); // The Catcher in the Rye
+// console.log(book2.name); // The Hitchhiker's Guide to the Galaxy
+
+
+
+// function nickMakesTheBestFunctions(x, y) {
+//     return x * y;
+// }
+
+
+// console.out(nickMakesTheBestFunctions(4, 10));
+// console.out(nickMakesTheBestFunctions(5, -2));
